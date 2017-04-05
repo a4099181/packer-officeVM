8400b2953e7814bcd32d383445b5cd3304d852ff
seb! <sebi@sebi.one.pl>
2017-04-06 01:13:20 +0200

    Complete set of files to box Windows 10 1607 for ...
    vagrant-officeVM @ https://github.com/a4099181/vagrant-officeVM.

---
+++ build.ps1
@@ -0,0 +1,18 @@
+Param( [Parameter(Mandatory)] [String] $isoUrl,
+                              [String] $os = 'windows10_1607' )
+
+Function Invoke-Packer
+{
+    Param( [ValidateSet('windows10_1607')]
+           [Parameter(Mandatory)] [String] $os,
+           [Parameter(Mandatory)] [String] $isoUrl,
+           [Parameter(Mandatory)] [String] $isoMd5 )
+
+    packer build `
+        --var iso_url="$isoUrl" `
+        --var iso_checksum=$isoMd5 `
+        --var autounattend=.\$os\Autounattend.xml `
+        --only=virtualbox-iso .\$os\template.json
+}
+
+Invoke-Packer $os $isoUrl ( Get-FileHash $isoUrl -Algorithm MD5 ).Hash
