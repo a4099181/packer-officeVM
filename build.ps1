1ca1fc0087b82ff6f1954bbe545496ee3142869b
seb! <sebi@sebi.one.pl>
2017-04-07 23:16:02 +0200

    [Integration with vagrant-officeVM] build.ps1 update.

--- build.ps1
+++ build.ps1
@@ -1,10 +1,10 @@
 Param( [Parameter(Mandatory)] [String] $isoUrl,
-                              [String] $os     = 'windows10_1703',
-                              [String] $locale = 'pl-PL' )
+                              [String] $os      = 'windows-10.1703',
+                              [String] $locale  = 'pl-PL')
 
 Function Invoke-Packer
 {
-    Param( [ValidateSet('windows10_1607','windows10_1703')]
+    Param( [ValidateSet('windows-10.1607','windows-10.1703')]
            [Parameter(Mandatory)] [String] $os,
            [ValidateSet('pl-PL')]
            [Parameter(Mandatory)] [String] $locale,
@@ -16,6 +16,41 @@ Function Invoke-Packer
         --var iso_checksum=$isoMd5 `
         --var autounattend=.\$os\$locale\Autounattend.xml `
         --only=virtualbox-iso .\$os\template.json
+
+    if ( -Not $? )
+    {
+        throw "Non-zero exit code from packer (exit code: $lastExitCode). See packer output."
+    }
 }
 
-Invoke-Packer $os $locale $isoUrl ( Get-FileHash $isoUrl -Algorithm MD5 ).Hash
+Function Step-BuildVersion
+{
+    Param( [Parameter(Mandatory)] [String] $os,
+                                  [String] $boxFile = "packed\$os.json",
+                                  [String] $default = @"
+    { "name"     : "packer-officeVM",
+      "versions" : [{
+                 "version"   : "$($os.Split("-")[1])",
+                 "providers" : [{
+                             "name" : "virtualbox",
+                             "url"  : "packer-officeVM/packed/$os.virtualbox.box"
+                 }]
+      }]
+    }
+"@ )
+
+    ( $(If (Test-Path $boxFile) { Get-Content $boxFile } Else { $default }) | ConvertFrom-Json ) |
+        % {
+            $box = $_
+            $box.versions |
+            % {
+                $_.version = ( New-Object Version $_.version |
+                    % { New-Object Version $_.Major, $_.Minor, ( $_.Build + 1 ) } ).ToString(3)
+            }
+
+            $box | ConvertTo-Json -Compress -Depth 4 | Out-File -Encoding ascii $boxFile
+        }
+}
+
+Invoke-Packer $os $locale $isoUrl ( Get-FileHash $isoUrl -Algorithm MD5 ).Hash -ErrorAction Stop
+Step-BuildVersion $os
